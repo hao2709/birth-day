52ec4140939a9d94cd3aaf877e2e8017dbea70b7
hao2709 <63964110+hao2709@users.noreply.github.com>
2024-09-28 22:34:23 +0700

    birthday

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc em ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'huyentrinh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
